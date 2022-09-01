57d0c0799df1fb5756e1aff9d39aedec6335de1d
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-09-01 08:48:09 -0400

    Remove unneeded example code

--- src/main/java/org/embeddedt/tinkerleveling/TinkerLeveling.java
+++ src/main/java/org/embeddedt/tinkerleveling/TinkerLeveling.java
@@ -110,23 +110,4 @@ public class TinkerLeveling {
     public static void registerSoundEvent(RegistryEvent.Register<SoundEvent> event) {
         event.getRegistry().register(SOUND_LEVELUP);
     }
-
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // Register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
 }
